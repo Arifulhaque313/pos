c5760f4726b1de82355256ece947e76df329a0d9
Arifulhaque313 <asajib7654@gmail.com>
2023-02-10 21:30:37 +0600

    add: init *

---
+++ config/author.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Company/Author/Developer/Licence information of the application
+    |--------------------------------------------------------------------------
+    |
+    | All details about the author/developer/contact/licence of the application
+    |
+    | IMPORTANT: CHANGING ANY OF THIS INFORMATION WILL UNSTABILIZE THE APPLICATION
+    |
+    */
+
+    'vendor' => '',
+    'vendor_url' => '',
+    'email' => '',
+    'app_version' => "4.7.8",
+    'lic1' => '',
+    'pid' => 1,
+    'envato_purchase_code' => env('ENVATO_PURCHASE_CODE', 0)
+];
